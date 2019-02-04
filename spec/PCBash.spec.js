3c1b2a92b65ee5c2793f92034dc7f494af74afa8
Marc Smith <mr.marcsmith@me.com>
2019-02-04 16:45:20 -0700

    'putStringInFile' also accepts objects

--- spec/PCBash.spec.js
+++ spec/PCBash.spec.js
@@ -106,5 +106,65 @@ describe('PCBash.putStringInFile', () => {
 			.catch(done.fail);
 	});
 
+
+	it('should handle a js object and input as JSON', (done) => {
+		const path = '/tmp/spec-PCBash-putStringInFile-EOF-new-lines'
+
+
+		const contents = {}
+		contents.keyString = "value";
+		contents.keyNumber = 5;
+		contents.keyDate = new Date();
+		contents.keyNull = null;
+		const array = [];
+		array.push("hi");
+		array.push(1);
+		array.push(new Date())
+		array.push(null)
+		contents.arrayKey = array
+		const subDic = {}
+		subDic.subKeyString = "hi";
+		subDic.subKeyNumber = 5;
+		subDic.subKeyDate = new Date();
+		subDic.subKeyNull = null;
+		contents.subDic = subDic;
+		const contentCheck = JSON.stringify(contents);
+
+		PCBash.putStringInFile(contents,path)
+			.then((result)=>{
+				expect(result).toBe("");
+				const command = 'cat "' + path + '"';
+				return PCBash.runCommandPromise(command)
+					.then((result)=>{
+						expect(result).toBeDefined();
+						expect(result).toBe(contentCheck);
+					})
+			})
+			.then(()=>{
+				// clean up
+				// make sure path isn't bad news
+				expect(path).toContain("/tmp/")
+				const command = 'rm ' + path;
+				return PCBash.runCommandPromise(command)
+					.then((result)=>{
+						expect(result).toBeDefined();
+						expect(result).toBe('');
+					})
+			})
+			.then(()=>{
+				// confirm clean up
+				const command = 'cat "' + path + '"';
+				return PCBash.runCommandPromise(command)
+					.then(()=>{
+						expect(true).toBe(false);
+					})
+					.catch((error)=>{
+						expect(error).toContain('No such file or directory');
+						done();
+					});
+			})
+			.catch(done.fail);
+	});
+
 });
 
--- src/PCBash.js
+++ src/PCBash.js
@@ -5,7 +5,8 @@ class PCBash  {
 		//Empty Constructor
 	}
 	static putStringInFile(file,path){
-
+		// stringify anything that isn't already a string
+		if(typeof file !== "string") file = JSON.stringify(file);
 		const fullCommand = "cat <<'EOFSSHFILE' > " + path + "\n" + file + "\nEOFSSHFILE\n";
 		return PCBash.runCommandPromise(fullCommand);
 	}
